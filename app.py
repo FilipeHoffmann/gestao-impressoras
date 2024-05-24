0dd80ed0cc92432156c4de8e04ef05f6ee1172ba
Filipe Antonio Hoffmann <103908208+FilipeHoffmann@users.noreply.github.com>
2024-05-24 11:16:46 -0300

    teste

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request
-from scripts import banco_de_dados
 
 app = Flask(__name__)
 
@@ -10,9 +9,7 @@ def index():
     
 @app.route('/contratos',methods=['GET'])
 def contratos():
-    lista = [['teste']]
-    return render_template('/contratos/contratos.html',
-                           contratos=lista)
+    return render_template('/contratos/contratos.html')
 
 if __name__ == "__main__":
     app.run(host="192.168.0.159",port=3000,debug=True)
