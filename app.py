63b225d7511a17f1f54016a92e0a3bcaf97c086e
Filipe Antonio Hoffmann <103908208+FilipeHoffmann@users.noreply.github.com>
2024-05-27 11:17:26 -0300

    Função aditivos adicionada

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ def index():
     if request.method == 'GET':
         return render_template('/home/index.html')
     
+#Contratos
+    
 @app.route('/contratos',methods=['GET','POST'])
 def contratos():
     if request.method == 'GET':
@@ -59,7 +61,58 @@ def criar_contrato():
         return render_template('/contratos/criar_contrato.html',
                                mensagem = mensagem)
     else:
-        return render_template('/contratos/criar_contrato.html')   
+        return render_template('/contratos/criar_contrato.html')
+    
+#Aditivos
+    
+@app.route('/contratos/aditivos',methods=['GET','POST'])
+def aditivos():
+    if request.method == 'GET':
+        lista_aditivos = contratos_model_instance.consultar_contratos("SELECT * FROM contractamendments ORDER BY amendment_id DESC")
+        return render_template('/contratos/aditivos/aditivos.html',
+                            aditivos = lista_aditivos,
+                            ids=lista_aditivos)
+        
+@app.route('/contratos/aditivos/criar', methods=['GET','POST'])
+def criar_aditivo():
+    if request.method == 'POST':
+        id_contrato = request.form['id_contrato']
+        data_prorrogada = request.form['data_prorrogada']
+        detalhes = request.form['detalhes']
+        custo_adicional = request.form['custo_adicional']
+        contratos_model_instance.criar_contrato(f'INSERT INTO contractamendments(contract_id,amendment_date,details,additional_cost) VALUES ("{id_contrato}","{data_prorrogada}","{detalhes}","{custo_adicional}")')
+        mensagem = "Contrato adicionado!"
+        return render_template('/contratos/aditivos/criar_aditivo.html',
+                               mensagem = mensagem)
+    else:
+        return render_template('/contratos/aditivos/criar_aditivo.html')
+    
+@app.route('/contratos/aditivos/editar/<int:id_aditivo>', methods=['GET', 'POST'])
+def editar_aditivo(id_aditivo):
+    aditivos = contratos_model_instance.consultar_contratos(f"SELECT * FROM contractamendments WHERE amendment_id = {id_aditivo}")
+    
+    if request.method == 'GET':
+        return render_template('/contratos/aditivos/editar_aditivo.html',  aditivo=aditivos)
+    
+    if request.method == 'POST':
+        id_contrato = request.form['id_contrato']
+        data_prorrogada = request.form['data_prorrogada']
+        detalhes = request.form['detalhes']
+        custo_adicional = request.form['custo_adicional']
+
+        update_query = f"""
+            UPDATE contractamendments
+            SET contract_id = '{id_contrato}', amendment_date = '{data_prorrogada}', details = '{detalhes}', additional_cost = '{custo_adicional}'
+            WHERE amendment_id = '{id_aditivo}'
+        """
+        contratos_model_instance.atualizar_contrato(update_query)
+        return redirect(url_for('aditivos'))
+    
+@app.route('/contratos/aditivos/excluir/<int:id_aditivo>', methods=['GET'])
+def excluir_aditivo(id_aditivo):
+    delete_query = f"DELETE FROM contractamendments WHERE amendment_id = {id_aditivo}"
+    contratos_model_instance.excluir_contrato(delete_query)
+    return redirect(url_for('aditivos'))
             
 if __name__ == "__main__":
     app.run(debug=True)
