0d65a73e42364e51a5e7da9582b324ab9bfd2d44
Filipe Antonio Hoffmann <103908208+FilipeHoffmann@users.noreply.github.com>
2024-05-27 14:33:11 -0300

    atualização do modelo e do app.py

--- app.py
+++ app.py
@@ -15,14 +15,14 @@ def index():
 @app.route('/contratos',methods=['GET','POST'])
 def contratos():
     if request.method == 'GET':
-        lista_contratos = contratos_model_instance.consultar_contratos("SELECT * FROM contracts ORDER BY contract_id DESC")
+        lista_contratos = contratos_model_instance.consultar_contratos("SELECT * FROM contratos ORDER BY id_contratos DESC")
         return render_template('/contratos/contratos.html',
                             contratos = lista_contratos,
                             ids=lista_contratos)
         
 @app.route('/contratos/editar/<int:id_contrato>', methods=['GET', 'POST'])
 def editar_contrato(id_contrato):
-    contrato = contratos_model_instance.consultar_contratos(f"SELECT * FROM contracts WHERE contract_id = {id_contrato}")
+    contrato = contratos_model_instance.consultar_contratos(f"SELECT * FROM contratos WHERE id_contratos = {id_contrato}")
     
     if request.method == 'GET':
         return render_template('contratos/editar_contrato.html', contracts_id=id_contrato, contrato=contrato)
@@ -35,16 +35,16 @@ def editar_contrato(id_contrato):
         custo_total = request.form['custo_total']
 
         update_query = f"""
-            UPDATE contracts
-            SET description = '{descricao}', start_date = '{data_inicio}', end_date = '{data_fim}', vendor = '{contratada}', total_cost = '{custo_total}'
-            WHERE contract_id = '{id_contrato}'
+            UPDATE contratos
+            SET descricao = '{descricao}', data_inicial = '{data_inicio}', data_final = '{data_fim}', contratada = '{contratada}', custo_total = '{custo_total}'
+            WHERE id_contratos = '{id_contrato}'
         """
         contratos_model_instance.atualizar_contrato(update_query)
         return redirect(url_for('contratos'))
     
 @app.route('/contratos/excluir/<int:id_contrato>', methods=['GET'])
 def excluir_contrato(id_contrato):
-    delete_query = f"DELETE FROM contracts WHERE contract_id = {id_contrato}"
+    delete_query = f"DELETE FROM contratos WHERE id_contratos = {id_contrato}"
     contratos_model_instance.excluir_contrato(delete_query)
     return redirect(url_for('contratos'))
         
@@ -56,7 +56,7 @@ def criar_contrato():
         data_fim = request.form['data_fim']
         contratada = request.form['contratada']
         custo_total = request.form['custo_total']
-        contratos_model_instance.criar_contrato(f'INSERT INTO contracts(description,end_date,start_date,total_cost,vendor) VALUES ("{descricao}","{data_fim}","{data_inicio}","{custo_total}","{contratada}")')
+        contratos_model_instance.criar_contrato(f'INSERT INTO contratos(descricao,data_final,data_inicial,custo_total,contratada) VALUES ("{descricao}","{data_fim}","{data_inicio}","{custo_total}","{contratada}")')
         mensagem = "Contrato adicionado!"
         return render_template('/contratos/criar_contrato.html',
                                mensagem = mensagem)
@@ -68,7 +68,7 @@ def criar_contrato():
 @app.route('/contratos/aditivos',methods=['GET','POST'])
 def aditivos():
     if request.method == 'GET':
-        lista_aditivos = contratos_model_instance.consultar_contratos("SELECT * FROM contractamendments ORDER BY amendment_id DESC")
+        lista_aditivos = contratos_model_instance.consultar_contratos("SELECT * FROM aditivos ORDER BY id_aditivos DESC")
         return render_template('/contratos/aditivos/aditivos.html',
                             aditivos = lista_aditivos,
                             ids=lista_aditivos)
@@ -80,7 +80,7 @@ def criar_aditivo():
         data_prorrogada = request.form['data_prorrogada']
         detalhes = request.form['detalhes']
         custo_adicional = request.form['custo_adicional']
-        contratos_model_instance.criar_contrato(f'INSERT INTO contractamendments(contract_id,amendment_date,details,additional_cost) VALUES ("{id_contrato}","{data_prorrogada}","{detalhes}","{custo_adicional}")')
+        contratos_model_instance.criar_contrato(f'INSERT INTO aditivos(id_contratos,data_aditivo,detalhes,custo_adicional) VALUES ("{id_contrato}","{data_prorrogada}","{detalhes}","{custo_adicional}")')
         mensagem = "Contrato adicionado!"
         return render_template('/contratos/aditivos/criar_aditivo.html',
                                mensagem = mensagem)
@@ -89,7 +89,7 @@ def criar_aditivo():
     
 @app.route('/contratos/aditivos/editar/<int:id_aditivo>', methods=['GET', 'POST'])
 def editar_aditivo(id_aditivo):
-    aditivos = contratos_model_instance.consultar_contratos(f"SELECT * FROM contractamendments WHERE amendment_id = {id_aditivo}")
+    aditivos = contratos_model_instance.consultar_contratos(f"SELECT * FROM aditivos WHERE id_aditivos = {id_aditivo}")
     
     if request.method == 'GET':
         return render_template('/contratos/aditivos/editar_aditivo.html',  aditivo=aditivos)
@@ -101,16 +101,16 @@ def editar_aditivo(id_aditivo):
         custo_adicional = request.form['custo_adicional']
 
         update_query = f"""
-            UPDATE contractamendments
-            SET contract_id = '{id_contrato}', amendment_date = '{data_prorrogada}', details = '{detalhes}', additional_cost = '{custo_adicional}'
-            WHERE amendment_id = '{id_aditivo}'
+            UPDATE aditivos
+            SET id_contratos = '{id_contrato}', data_aditivo = '{data_prorrogada}', detalhes = '{detalhes}', custo_adicional = '{custo_adicional}'
+            WHERE id_aditivo = '{id_aditivo}'
         """
         contratos_model_instance.atualizar_contrato(update_query)
         return redirect(url_for('aditivos'))
     
 @app.route('/contratos/aditivos/excluir/<int:id_aditivo>', methods=['GET'])
 def excluir_aditivo(id_aditivo):
-    delete_query = f"DELETE FROM contractamendments WHERE amendment_id = {id_aditivo}"
+    delete_query = f"DELETE FROM aditivos WHERE id_aditivos = {id_aditivo}"
     contratos_model_instance.excluir_contrato(delete_query)
     return redirect(url_for('aditivos'))
             
