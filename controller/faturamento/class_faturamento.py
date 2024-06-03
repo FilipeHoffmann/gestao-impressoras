d3b04e77e175f2756bba23a2a484eec292f5a705
Filipe Antonio Hoffmann <filipeantoniohoffmann@gmail.com>
2024-06-03 16:25:50 -0300

    Alteração na visualização dos faturamentos

--- controller/faturamento/class_faturamento.py
+++ controller/faturamento/class_faturamento.py
@@ -4,7 +4,31 @@ from static.scripts import conector_banco_de_dados
 class class_faturamento:
     @staticmethod
     def faturamento():
-        return render_template("/faturamento/faturamento.html")
+        query = f'''
+        SELECT c.id_contadores, secretarias.nome, setores.nome_setor, c.paginas_impressas,cotas.cota_mensal,produtos.valor_unitario
+        FROM contadores AS c
+        INNER JOIN setores ON c.id_setores = setores.id_setores
+        INNER JOIN secretarias ON setores.id_secretarias = secretarias.id_secretarias
+        INNER JOIN impressoras ON c.id_impressoras = impressoras.id_impressoras
+        INNER JOIN cotas ON impressoras.id_cotas = cotas.id_cotas
+        INNER JOIN produtos ON produtos.id_produtos= c.id_produtos
+        ORDER BY secretarias.nome ASC'''
+        lista_contadores = conector_banco_de_dados.conector_banco_de_dados(query).consultar()
+        custo_total = 0
+        #Adicionando a coluna EXCEDENTES e TOTAL A PAGAR
+        for i in range(len(lista_contadores)):
+            lista_contadores[i] = list(lista_contadores[i])
+            if (lista_contadores[i][3]-lista_contadores[i][4]) <= 0:
+                lista_contadores[i].append(0)
+                lista_contadores[i].append(0.0)
+            else:
+                lista_contadores[i].append(lista_contadores[i][3]-lista_contadores[i][4])
+                lista_contadores[i].append(lista_contadores[i][5]*lista_contadores[i][6])
+            custo_total += lista_contadores[i][7]
+        print(lista_contadores)
+        return render_template("/faturamento/faturamento.html",
+                               faturamentos = lista_contadores,
+                               custo_total=custo_total)
     
     def obter_faturamento(mes):
         query = f'''
