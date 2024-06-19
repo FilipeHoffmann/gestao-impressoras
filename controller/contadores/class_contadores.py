9d00b883eb0121004f69ba7ef552c5db2947748c
Filipe Antonio Hoffmann <103908208+FilipeHoffmann@users.noreply.github.com>
2024-06-19 10:43:41 -0300

    Atualização

--- controller/contadores/class_contadores.py
+++ controller/contadores/class_contadores.py
@@ -7,12 +7,18 @@ class class_contadores:
         if request.method == "GET":
             if mes == None:
                 query = conector_banco_de_dados.conector_banco_de_dados(f'''
-                SELECT *
-                FROM contadores''')
+                SELECT contadores.id_contadores, contadores.id_impressoras, secretarias.nome, setores.nome_setor, contadores.data_coleta, contadores.paginas_impressas, contadores.mes_referente, contadores.proporcao_mes 
+                FROM contadores
+                INNER JOIN impressoras ON impressoras.id_impressoras = contadores.id_impressoras
+                INNER JOIN setores ON setores.id_setores = impressoras.id_setores
+                INNER JOIN secretarias ON secretarias.id_secretarias = setores.id_secretarias''')
             else:
                 query = conector_banco_de_dados.conector_banco_de_dados(f'''
-                SELECT *
+                SELECT contadores.id_contadores, contadores.id_impressoras, secretarias.nome, setores.nome_setor, contadores.data_coleta, contadores.paginas_impressas, contadores.mes_referente, contadores.proporcao_mes
                 FROM contadores
+                INNER JOIN impressoras ON impressoras.id_impressoras = contadores.id_impressoras
+                INNER JOIN setores ON setores.id_setores = impressoras.id_setores
+                INNER JOIN secretarias ON secretarias.id_secretarias = setores.id_secretarias
                 WHERE "{mes}" = mes_referente''')
                 
             contadores = query.consultar()
@@ -28,8 +34,9 @@ class class_contadores:
             data_coleta = request.form["data_coleta"]
             paginas_impressas = request.form["paginas_impressas"]
             mes_referente = request.form["mes_referente"]
-            query = f'''INSERT INTO contadores(id_impressoras,id_setores,id_produtos,data_coleta,paginas_impressas,mes_referente)
-                    VALUES ('{id_impressoras}','{id_setores}','{id_produtos}','{data_coleta}','{paginas_impressas}','{mes_referente}')'''
+            proporcao_mes = request.form["proporcao_mes"]
+            query = f'''INSERT INTO contadores(id_impressoras,id_setores,id_produtos,data_coleta,paginas_impressas,mes_referente,proporcao_mes)
+                    VALUES ('{id_impressoras}','{id_setores}','{id_produtos}','{data_coleta}','{paginas_impressas}','{mes_referente}','{proporcao_mes}')'''
             conector_banco_de_dados.conector_banco_de_dados(query).alterar_incluir_excluir()
             mensagem = "Contador adicionado!"
             return render_template("/contadores/criar_contador.html",
@@ -53,9 +60,10 @@ class class_contadores:
         data_coleta = request.form["data_coleta"]
         paginas_impressas = request.form["paginas_impressas"]
         mes_referente = request.form["mes_referente"]
+        proporcao_mes= request.form["proporcao_mes"]
         update_query = f"""
             UPDATE contadores
-            SET id_impressoras = '{id_impressoras}', data_coleta = '{data_coleta}', paginas_impressas = '{paginas_impressas}', mes_referente = '{mes_referente}'
+            SET id_impressoras = '{id_impressoras}', data_coleta = '{data_coleta}', paginas_impressas = '{paginas_impressas}', mes_referente = '{mes_referente}', proporcao_mes = '{proporcao_mes}'
             WHERE id_contadores = '{id_contador}'
         """
         conector_banco_de_dados.conector_banco_de_dados(update_query).alterar_incluir_excluir()
@@ -70,7 +78,8 @@ class class_contadores:
                     'id_impressoras': contador[1],
                     'data_coleta': contador[2],
                     'paginas_impressas': contador[3],
-                    'mes_referente': contador[4]
+                    'mes_referente': contador[4],
+                    'proporcao_mes': contador[5]
                 }
                 return render_template('contadores/editar_contador.html', contador=contador_dict)
     
--- controller/impressoras/class_impressoras.py
+++ controller/impressoras/class_impressoras.py
@@ -4,18 +4,50 @@ from static.scripts import conector_banco_de_dados
 class class_impressoras:
     @staticmethod
     def impressoras():
-        query = conector_banco_de_dados.conector_banco_de_dados("SELECT * FROM impressoras ORDER BY id_impressoras DESC")
+        query = conector_banco_de_dados.conector_banco_de_dados('''SELECT `impressoras`.`id_impressoras`,
+        `impressoras`.`modelo`,
+        `impressoras`.`marca`,
+        `impressoras`.`ip`,
+        `impressoras`.`localizacao`,
+        `impressoras`.`status`,
+        `impressoras`.`tipo_impressora`,
+        `impressoras`.`cotas`,
+        `impressoras`.`contador_inicial`,
+        `impressoras`.`data_instalacao`,
+        `setores`.`nome_setor`,
+        `produtos`.`descricao`
+        FROM `controleimpressora`.`impressoras`
+        INNER JOIN setores ON setores.id_setores = impressoras.id_setores
+        INNER JOIN produtos ON produtos.id_produtos = impressoras.id_produtos;
+        ''')
         lista_impressoras = query.consultar()
         return render_template('/impressoras/impressoras.html',
                             impressoras = lista_impressoras,
                             ids=lista_impressoras)
 
     def obter_impressora(id_impressora):
-        query = conector_banco_de_dados.conector_banco_de_dados(f"SELECT * FROM impressoras WHERE id_impressoras = {id_impressora}")
+        query = conector_banco_de_dados.conector_banco_de_dados(f'''SELECT `impressoras`.`id_impressoras`,
+        `impressoras`.`modelo`,
+        `impressoras`.`marca`,
+        `impressoras`.`ip`,
+        `impressoras`.`localizacao`,
+        `impressoras`.`status`,
+        `impressoras`.`tipo_impressora`,
+        `impressoras`.`cotas`,
+        `impressoras`.`contador_inicial`,
+        `impressoras`.`data_instalacao`,
+        `setores`.`nome_setor`,
+        `produtos`.`descricao`
+        FROM `controleimpressora`.`impressoras`
+        INNER JOIN setores ON setores.id_setores = impressoras.id_setores
+        INNER JOIN produtos ON produtos.id_produtos = impressoras.id_produtos
+        WHERE id_impressoras = {id_impressora}''')
         impressoras = query.consultar()
         impressoras = impressoras[0]
         produtos_query = "SELECT id_produtos, descricao FROM produtos"
         produtos = conector_banco_de_dados.conector_banco_de_dados(produtos_query).consultar()
+        setores_query = "SELECT id_setores, nome_setor FROM setores"
+        setores = conector_banco_de_dados.conector_banco_de_dados(setores_query).consultar()
         impressoras_dict = {
             'modelo': impressoras[1],
             'marca': impressoras[2],
@@ -25,10 +57,11 @@ class class_impressoras:
             'tipo_impressora': impressoras[6],
             'cotas': impressoras[7],
             'contador_inicial': impressoras[8],
-            'id_setores': impressoras[9],
-            'id_produtos': impressoras[10]
+            'data_instalacao': impressoras[9],
+            'id_setores': impressoras[10],
+            'id_produtos': impressoras[11]
         }
-        return render_template('impressoras/editar_impressora.html', id_impressora=id_impressora, impressora=impressoras_dict, produtos=produtos)
+        return render_template('impressoras/editar_impressora.html', id_impressora=id_impressora, impressora=impressoras_dict, produtos=produtos, setores=setores)
     
     def editar_impressora(id_impressora):
         modelo = request.form['modelo']
@@ -40,10 +73,11 @@ class class_impressoras:
         id_setores = request.form['id_setores']
         cotas = request.form['cotas']
         contador_inicial = request.form['contador_inicial']
+        data_instalacao = request.form['data_instalacao']
         id_produtos = request.form['id_produtos']
         update_query = f"""
         UPDATE impressoras
-        SET modelo = '{modelo}', marca = '{marca}', ip = '{ip}', localizacao = '{localizacao}', status = '{status}', tipo_impressora = '{tipo_impressora}', id_setores = '{id_setores}', cotas = '{cotas}', contador_inicial = '{contador_inicial}', id_produtos = '{id_produtos}'
+        SET modelo = '{modelo}', marca = '{marca}', ip = '{ip}', localizacao = '{localizacao}', status = '{status}', tipo_impressora = '{tipo_impressora}', id_setores = '{id_setores}', cotas = '{cotas}', contador_inicial = '{contador_inicial}', data_instalacao = '{data_instalacao}', id_produtos = '{id_produtos}'
         WHERE id_impressoras = '{id_impressora}'
         """
         conector_banco_de_dados.conector_banco_de_dados(update_query).alterar_incluir_excluir()
@@ -65,9 +99,10 @@ class class_impressoras:
         id_setores = request.form['id_setores']
         cotas = request.form['cotas']
         contador_inicial = request.form['contador_inicial']
+        data_instalacao = request.form['data_instalacao']
         id_produtos = request.form['id_produtos']
         
-        conector_banco_de_dados.conector_banco_de_dados(f'INSERT INTO impressoras(id_impressoras, modelo, marca, ip, localizacao, status, tipo_impressora, id_setores, cotas, contador_inicial, id_produtos) VALUES ("{id_impressora}","{modelo}","{marca}","{ip}","{localizacao}","{status}", "{tipo_impressora}","{id_setores}", "{cotas}", "{contador_inicial}", "{id_produtos}")').alterar_incluir_excluir()
+        conector_banco_de_dados.conector_banco_de_dados(f'INSERT INTO impressoras(id_impressoras, modelo, marca, ip, localizacao, status, tipo_impressora, id_setores, cotas, contador_inicial, data_instalacao, id_produtos) VALUES ("{id_impressora}","{modelo}","{marca}","{ip}","{localizacao}","{status}", "{tipo_impressora}","{id_setores}", "{cotas}", "{contador_inicial}","{data_instalacao}","{id_produtos}")').alterar_incluir_excluir()
         #Criar o contador vazio
         meses = ["JANEIRO","FEVEREIRO","MARÇO","ABRIL","MAIO","JUNHO","JULHO","AGOSTO","SETEMBRO","OUTUBRO","NOVEMBRO","DEZEMBRO"]
         for i in range(len(meses)):
