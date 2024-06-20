2e2a30c16697ebc06f9d95a736943238fd0e4e52
Filipe Antonio Hoffmann <103908208+FilipeHoffmann@users.noreply.github.com>
2024-06-20 11:54:47 -0300

    Atualização

--- controller/faturamento/class_faturamento.py
+++ controller/faturamento/class_faturamento.py
@@ -15,21 +15,7 @@ class class_faturamento:
         WHERE impressoras.status = "ATIVA" OR impressoras.status = "TROCADA"
         ORDER BY secretarias.nome ASC
         '''
-        lista_contadores = conector_banco_de_dados.conector_banco_de_dados(query).consultar()
-        custo_total = 0
-        #Adicionando a coluna EXCEDENTES e TOTAL A PAGAR
-        for i in range(len(lista_contadores)):
-            lista_contadores[i] = list(lista_contadores[i])
-            if (lista_contadores[i][6]-lista_contadores[i][7]) <= 0:
-                lista_contadores[i].append(0)
-                lista_contadores[i].append(0.0)
-            else:
-                lista_contadores[i].append(lista_contadores[i][6]-lista_contadores[i][7])
-                lista_contadores[i].append(lista_contadores[i][8]*lista_contadores[i][9])
-            custo_total += lista_contadores[i][10]
-        return render_template("/faturamento/faturamento.html",
-                               faturamentos = lista_contadores,
-                               custo_total=custo_total)
+        return render_template("/faturamento/faturamento.html")
     
     def obter_faturamento(mes):
         query = f'''
@@ -94,6 +80,7 @@ class class_faturamento:
                 lista_contadores[i].append(lista_contadores[i][6]-lista_contadores[i][7])
                 lista_contadores[i].append(lista_contadores[i][8]*lista_contadores[i][9])
                 
-        obj_salvar = salvar_dados(lista_contadores,f"relatorio_{mes}.csv")
+    def salvar_dados(lista_contadores:list, mes:str):
+        obj_salvar:object = salvar_dados(lista_contadores,f"relatorio_{mes}.csv")
         obj_salvar.salvar()
         return send_file(f"temp/relatorio_{mes}.csv")
